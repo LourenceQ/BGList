c8cc554a113485c5415075153a03a4d6b79bf2aa
LourenceQ <lawrenceqf@gmail.com>
2023-04-01 07:39:40 -0300

    Implementing middlewares to expose friendly development error page when in development env

--- MyBgList/MyBgList/Program.cs
+++ MyBgList/MyBgList/Program.cs
@@ -5,21 +5,41 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// resolve conflitos de rotas duplicadas automaticamente
+builder.Services.AddSwaggerGen(opts => opts.ResolveConflictingActions(apiDesc => apiDesc.First()));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}*/
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+// using minimal api approach to get the same result as controller based api
+app.MapGet("/error", () => Results.Problem());
+// emulating an exception
+app.MapGet("/error/test", () => { throw new Exception("test"); });
 
 app.Run();
