7bd723e627b74e4e517e103b2cf498b75c8d498b
LourenceQ <lawrenceqf@gmail.com>
2023-05-04 22:29:40 -0300

    Implementando model de context

---
+++ MyBgList/MyBgList/Models/BoardGame.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyBgList.Models;
+
+[Table("BoardGames")]
+public class BoardGame
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = null!;
+    [Required]
+    public int Year { get; set; }
+    [Required]
+    public int MinPlayers { get; set; }
+    [Required]
+    public int MaxPlayers { get; set; }
+    [Required]
+    public int PlayTime { get; set; }
+    [Required]
+    public int MinAge { get; set; }
+    [Required]
+    public int UsersRated { get; set; }
+    [Required]
+    [Precision(4, 2)]
+    public decimal RatingAverage { get; set; }
+    [Required]
+    public int BGGRank { get; set; }
+    [Required]
+    [Precision(4, 2)]
+    public decimal ComplexityAverage { get; set; }
+    [Required]
+    public int OwnedUsers { get; set; }
+    [Required]
+    public DateTime CreatedDate { get; set; }
+    [Required]
+    public DateTime LastModifiedDate { get; set; }
+}
