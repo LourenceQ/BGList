e07bce87319156332a9e0b97f3effea06506e399
LourenceQ <lawrenceqf@gmail.com>
2023-05-12 21:55:36 -0300

    Implementando controller de seedData

---
+++ MyBgList/MyBgList/Controllers/SeedController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBgList.Models;
+
+namespace MyBgList.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SeedController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    private readonly ILogger<SeedController> _logger;
+
+    private readonly IWebHostEnvironment _env;
+
+    public SeedController(ApplicationDbContext context
+        , ILogger<SeedController> logger
+        , IWebHostEnvironment env)
+    {
+        _context = context;
+        _logger = logger;
+        _env = env;
+    }
+}
