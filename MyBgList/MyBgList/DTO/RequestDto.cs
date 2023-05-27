3cca9acd16314c7276f6213a9b546733fcc4d2f3
LourenceQ <lawrenceqf@gmail.com>
2023-05-26 23:29:30 -0300

    modificação em dto de reques par se tornar tipo generico

--- MyBgList/MyBgList/DTO/RequestDto.cs
+++ MyBgList/MyBgList/DTO/RequestDto.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 
 namespace MyBgList.DTO;
 
-public class RequestDto
+public class RequestDto<T> : IValidatableObject
 {
     [DefaultValue(0)]
     public int PageIndex { get; set; } = 0;
@@ -23,4 +23,13 @@ public class RequestDto
 
     [DefaultValue(null)]
     public string? FilterQuery { get; set; } = null;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var validator = new SortColumnValidatorAttribute(typeof(T));
+
+        var result = validator.GetValidationResult(SortColumn, validationContext);
+
+        return (result != null) ? new[] { result } : new ValidationResult[0];
+    }
 }
